a3d6aaa87bb70524207b7608aaefaf67bc21ca1f
石井勝久 <katsuchan@ishiikatsuhisa-no-MacBook-Air.local>
2019-04-23 18:18:52 +0900

    i->l

---
+++ a.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+inline double sqr(double v) {
+    return v * v;
+}
+
+int main(){
+    int i = 0,n,a[100];
+    double b=0.0,c=0.0;
+    while(cin >> n){
+        a[i] = n;
+        i++;
+    }
+    for(int l=0; l<i; l++){
+        b+=a[l];
+    } 
+    b /= i;
+    for (int l = 0; l < i; l++){
+        c += sqr(a[l] - b);
+    }
+    c/=i;
+    c=sqrt(c);
+
+    cout << "平均:" << b << " 標準偏差:" << c << endl;
+    return 0;
+}
